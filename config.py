3725b2993c8eefbe2af983c4507c5fba44fd8b11
Alexey Krytsky <alexeykrytsky@gmail.com>
2024-05-24 17:32:01 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Get the path of the directory where the current file is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATASETS_PATH = os.path.join(BASE_DIR, 'datasets')
+DEMO_DIR_PATH = os.path.join(BASE_DIR, 'demo')
